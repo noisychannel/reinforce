a86ec1690ec11240bf3661b13eb1c11bfa02c62d
Gaurav Kumar <gaurav.bison@gmail.com>
2016-09-04 18:27:04 -0400

    Removed pyc files

--- train.py
+++ train.py
@@ -9,9 +9,8 @@ from bandit import Bandit
 from learner import learner
 
 ARMS = 10
-#N_BANDITS = 2000
-N_BANDITS=1
-PLAYS = 100000
+N_BANDITS = 2000
+PLAYS = 1000
 
 # pylint: disable=C0103
 bandits = [Bandit() for _ in range(N_BANDITS)]
@@ -21,19 +20,23 @@ history_value, history_optimal = learner(bandits, PLAYS, 'greedy')
 history_value_0_01, history_optimal_0_01 = learner(bandits, PLAYS, 'greedy', eps=0.01)
 history_value_0_1, history_optimal_0_1 = learner(bandits, PLAYS, 'greedy', eps=0.1)
 
-#t_history_value, t_history_optimal = softmax_learn(bandits, plays, 1.)
-#t_history_value_10, t_history_optimal_10 = softmax_learn(bandits, plays, 10.)
-#t_history_value_100, t_history_optimal_100 = softmax_learn(bandits, plays, 100.)
-#print history_value
+average_oracle = [numpy.average([bandits[i].oracle_value for i in range(N_BANDITS)])] * PLAYS
 plt.plot([i for i in range(len(history_value))], average_oracle, label="oracle")
 plt.plot([i for i in range(len(history_value))], history_value, label="eps=0 (greedy)")
 plt.plot([i for i in range(len(history_value))], history_value_0_01, label="eps=0.01")
 plt.plot([i for i in range(len(history_value))], history_value_0_1, label="eps=0.1")
-#plt.plot([i for i in range(len(history_value))], t_history_value, label="tau=1")
-#plt.plot([i for i in range(len(history_value))], t_history_value_10, label="tau=10")
-#plt.plot([i for i in range(len(history_value))], t_history_value_100, label="tau=100")
 #plt.plot([i for i in range(len(history_value))], history_optimal, label="eps=0 (greedy)")
 #plt.plot([i for i in range(len(history_value))], history_optimal_0_01, label="eps=0.01")
 #plt.plot([i for i in range(len(history_value))], history_optimal_0_1, label="eps=0.1")
+
+N_BANDITS = 1
+bandits = [Bandit() for _ in range(N_BANDITS)]
+t_history_value, t_history_optimal = learner(bandits, PLAYS, 'softmax', tau=1.)
+t_history_value_0_1, t_history_optimal_0_1 = learner(bandits, PLAYS, 'softmax', tau=0.1)
+t_history_value_0_01, t_history_optimal_0_01 = learner(bandits, PLAYS, 'softmax', tau=0.01)
+plt.plot([i for i in range(len(history_value))], t_history_value, label="tau=1")
+plt.plot([i for i in range(len(history_value))], t_history_value_0_1, label="tau=0.1")
+plt.plot([i for i in range(len(history_value))], t_history_value_0_01, label="tau=0.01")
+
 plt.legend(loc=4)
 plt.show()
